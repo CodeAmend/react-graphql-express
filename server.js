6ce165ac6f701862ecd2247d1e43b2433a0ccc11
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-02-11 18:11:45 -0500

    Mongoose basic setup

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+require('dotenv').config({ path: 'variables.env' });
+const mongoose = require('mongoose');
 const express = require('express');
+
+const { PORT, MONGO_URI } = process.env;
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(MONGO_URI, mongooseOptions)
+  .then(() => console.log('DB connected'))
+  .catch(err => console.error(err));
+
+// Initialzes application
 const app = express();
-
-const PORT = process.env.PORT || 4444;
-
 
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
