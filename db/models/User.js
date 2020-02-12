c721fc57ca4e941edc5b23bf539adfd6cdcffc80
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-02-12 16:40:23 -0500

    Hash passpasword with User model

--- db/models/User.js
+++ db/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -28,6 +29,25 @@ const UserSchema = new Schema({
     type: [Schema.Types.ObjectId],
     ref: 'Recipe',
   },
+});
+
+UserSchema.pre('save', function(next) {
+  console.log("PRE");
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  console.log("GenSalt");
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    console.log("Hashing");
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  })
 });
 
 module.exports = mongoose.model('User', UserSchema);
