41be7d8f08951cbfba7b02cbc8222ea7172d1dbb
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-02-12 00:35:42 -0500

    Set up basic Graphql with Express

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 require('dotenv').config({ path: 'variables.env' });
+const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
 
+const { typeDefs } = require('./typeDefs');
+const { resolvers } = require('./resolvers');
+
 const { PORT, MONGO_URI } = process.env;
 
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Connects to Mongoose
 const mongooseOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,7 +25,9 @@ mongoose.connect(MONGO_URI, mongooseOptions)
   .catch(err => console.error(err));
 
 // Initialzes application
+
 const app = express();
+server.applyMiddleware({ app });
 
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
