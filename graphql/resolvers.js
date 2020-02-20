81f1f615983c2a02cf38b6d5735311e75813c5c7
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-02-20 15:15:27 -0500

    Add signin mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,16 +1,18 @@
 require('dotenv').config({ path: 'variables.env' });
 const jwt = require('jsonwebtoken');
-const SECRET = process.env.SECRET; // 'secret-key';
+const bcrypt = require('bcrypt');
 
-const createToken = (user, secret, expiresIn) => {
+
+const createToken = (user, expiresIn) => {
   const { username, email } = user;
+  const SECRET = process.env.SECRET;
 
-  return jwt.sign({ username, email }, secret, { expiresIn });
+  return jwt.sign({ username, email }, SECRET, { expiresIn });
 }
 
 exports.resolvers = {
   Query: {
-    allRecipes: async (root, args, { models }, info) => {
+    allRecipes: async (_, __, { models }) => {
       const { Recipe } = models;
       const recipes = await Recipe.find();
       return recipes;
@@ -18,7 +20,7 @@ exports.resolvers = {
   },
 
   Mutation: {
-    addRecipe: async (root, args, { models }, info) => {
+    addRecipe: async (_, args, { models }) => {
       const { name, category, description, instructions, username } = args;
       const { Recipe } = models;
 
@@ -29,7 +31,7 @@ exports.resolvers = {
       return newRecipe;
     },
 
-    signupUser: async (root, args, { models }, info) => {
+    signupUser: async (_, args, { models }) => {
       const { username, email, password } = args;
       console.log({ args });
       const { User } = models;
@@ -40,11 +42,31 @@ exports.resolvers = {
         throw new Error('User already exists');
       }
 
-      const newUser = await new User({ username, password, email }).save();
+      await new User({ username, password, email }).save();
 
       const expiresIn = 1000 * 60 * 1;
-      const token = createToken({ username, password }, SECRET, expiresIn);
+      const token = createToken({ username, password }, expiresIn);
 
+      return { token };
+    },
+
+    signinUser: async (_, args, { models }) => {
+      const { username, password } = args;
+      const { User } = models;
+
+      const user = User.find({ username });
+
+      if (!user) {
+        throw new Error("Not a valid username!");
+      }
+
+      const isValidPassword = await bcrypt.compare(password, user.password);
+
+      if (!isValidPassword) {
+        throw new Error("Not a valid password!");
+      }
+
+      const token = createToken(user, '1hr');
       return { token };
     }
   },
